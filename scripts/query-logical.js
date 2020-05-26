1a2d0bc8f5c875f81513cf6c7e588db98de37c95
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-05-26 12:10:59 +0700

    add query logical mongodb

---
+++ scripts/query-logical.js
@@ -0,0 +1,43 @@
+// select * from products where category in ('laptop', 'handphone') and price > 20000000
+db.products.find({
+    $and: [
+        {
+            category: {
+                $in: ["laptop", "handphone"]
+            }
+        },
+        {
+            price: {
+                $gt: 20000000
+            }
+        }
+    ]
+});
+
+// select * from products where category not in ('laptop', 'handphone')
+db.products.find({
+    category: {
+        $not: {
+            $in: ["laptop", "handphone"]
+        }
+    }
+});
+
+// select * from products where price between 10000000 and 20000000 and category != 'food'
+db.products.find({
+    $and: [
+        {
+            price: {
+                $gte: 10000000,
+                $lte: 20000000
+            }
+        },
+        {
+            category: {
+                $not: {
+                    $eq: 'food'
+                }
+            }
+        }
+    ]
+});
