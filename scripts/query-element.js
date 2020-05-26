de842fbc01a85ff93b7d330a58f4210a13f3c1e4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-05-26 13:36:59 +0700

    add $type operator

--- scripts/query-element.js
+++ scripts/query-element.js
@@ -4,3 +4,17 @@ db.products.find({
         $exists: false
     }
 });
+
+// select * from products where type(category) = "string"
+db.products.find({
+    category: {
+        $type: "string"
+    }
+});
+
+// select * from products where type(price) in ("int", "long")
+db.products.find({
+    price: {
+        $type: ["int", "long"]
+    }
+});
