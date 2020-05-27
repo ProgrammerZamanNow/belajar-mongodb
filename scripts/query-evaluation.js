f86595e75afe2e511a1bdd0d3a5ac5273538af02
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-05-27 12:44:11 +0700

    add query evaluation mongodb

--- scripts/query-evaluation.js
+++ scripts/query-evaluation.js
@@ -3,7 +3,7 @@ db.products.find({
     $expr: {
         $gt: ["$price", 1000000]
     }
-})
+});
 
 // select * from customers where toUpper(_id) = 'KHANNEDY'
 db.customers.find({
@@ -13,14 +13,14 @@ db.customers.find({
             "KHANNEDY"
         ]
     }
-})
+});
 
 // select * from products where name is not null and category is not null
 db.products.find({
     $jsonSchema: {
         required: [ "name", "category"]
     }
-})
+});
 
 // select * from products where name is not null and type(name) = 'string' and type(price) = 'long'
 db.products.find({
@@ -42,11 +42,45 @@ db.products.find({
     price: { 
         $mod: [5, 0]
     } 
-})
+});
 
+// select * from products where name like "%mie%"
+db.products.find({
+    name: {
+        $regex: /mie/,
+        $options: "i"
+    }
+});
 
+// select * from products where name like "Mie%"
+db.products.find({
+    name: {
+        $regex: /^Mie/
+    }
+});
 
+// create text index on products
+db.products.createIndex({
+    name: "text"
+});
 
+// select * from products where (name like "%mie%" or name like "%sedap%")
+db.products.find({
+    $text: {
+        $search: "mie sedap"
+    }
+});
 
+// select * from products where name like "%mie sedap%"
+db.products.find({
+    $text: {
+        $search: '"mie sedap"'
+    }
+});
 
-
+// select * fro customers where _id = "khannedy"
+db.customers.find({
+    $where: function(){
+        return this._id == "khannedy";
+    }
+});
