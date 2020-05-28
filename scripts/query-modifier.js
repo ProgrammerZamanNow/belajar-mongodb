552534e8970f97b15ef5c0a562609f5f0acd4131
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-05-29 00:01:45 +0700

    query modifier mongodb

---
+++ scripts/query-modifier.js
@@ -0,0 +1,14 @@
+// select count(*) from products
+db.products.find({}).count()
+
+// select * from products limit 4
+db.products.find({}).limit(4)
+
+// select * from products limit 4,2
+db.products.find({}).limit(4).skip(2)
+
+// select * from products order by name asc, category desc
+db.products.find({}).sort({
+    name: 1,
+    category: -1
+})
