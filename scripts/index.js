60d56e48eb4d35ebc66327b2546124fcab8ad33f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-05-31 01:57:14 +0700

    index mongodb

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+// Create index at category in products collection
+db.products.createIndex({
+    category: 1
+});
+
+// Get all indexes in products collection
+db.products.getIndexes();
+
+// Find products by category (will use index)
+db.products.find({
+    category: "food"
+});
+
+// Debugging query optimization
+db.products.find({
+    category: "food"
+}).explain();
+
+// Create index at price and tags in products collection
+db.products.createIndex({
+    stock: 1,
+    tags: 1
+});
+
+// Find products by stock and tags (will use index)
+db.products.find({
+    stock: 10,
+    tags: "popular"
+});
+
+// Debugging query optimization
+db.products.find({
+    stock: 10,
+    tags: "popular"
+}).explain();
