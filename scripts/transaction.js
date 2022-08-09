6c150e15ebdd4f288534f285aaff3e2107186c04
Yadi Apriyadi <83240362+YadBro@users.noreply.github.com>
2022-08-09 04:51:21 +0000

    NumberLong is Depracted!

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -1,11 +1,11 @@
 // Setting replication
 rs.initiate(
   {
-    _id : 'my-mongo-set',
+    _id: 'my-mongo-set',
     members: [
-      { _id : 0, host : "mongo1:27017" },
-      { _id : 1, host : "mongo2:27017" },
-      { _id : 2, host : "mongo3:27017" }
+      { _id: 0, host: "mongo1:27017" },
+      { _id: 1, host: "mongo2:27017" },
+      { _id: 2, host: "mongo3:27017" }
     ]
   }
 )
@@ -16,89 +16,89 @@ db.createCollection("orders");
 
 // Insert products
 db.products.insertMany([
-    {
-        _id: 1,
-        name: "Indomie Ayam Bawang",
-        price: new NumberLong(2000),
-        quantity: 10
-    },
-    {
-        _id: 2,
-        name: "Mie Sedap",
-        price: new NumberLong(2000),
-        quantity: 10
-    }
+  {
+    _id: 1,
+    name: "Indomie Ayam Bawang",
+    price: new Long(2000),
+    quantity: 10
+  },
+  {
+    _id: 2,
+    name: "Mie Sedap",
+    price: new Long(2000),
+    quantity: 10
+  }
 ]);
 
 // Sample abort transaction
-var session = db.getMongo().startSession( { readPreference: { mode: "primary" } } );
-session.startTransaction( { readConcern: { level: "majority" }, writeConcern: { w: "majority" } } );
+var session = db.getMongo().startSession({ readPreference: { mode: "primary" } });
+session.startTransaction({ readConcern: { level: "majority" }, writeConcern: { w: "majority" } });
 session.getDatabase("test").orders.insertOne({
-    _id: new ObjectId(),
-    total: new NumberLong(8000),
-    items: [
-        {
-            product_id: 1,
-            price: new NumberLong(2000),
-            quantity: new NumberInt(2)
-        },
-        {
-            product_id: 2,
-            price: new NumberLong(2000),
-            quantity: new NumberInt(2)
-        }
-    ]
+  _id: new ObjectId(),
+  total: new Long(8000),
+  items: [
+    {
+      product_id: 1,
+      price: new Long(2000),
+      quantity: new NumberInt(2)
+    },
+    {
+      product_id: 2,
+      price: new Long(2000),
+      quantity: new NumberInt(2)
+    }
+  ]
 });
 session.getDatabase("test").products.updateOne({
-    _id: 1
+  _id: 1
 }, {
-    $inc: {
-        quantity: -2
-    }
+  $inc: {
+    quantity: -2
+  }
 });
 session.getDatabase("test").products.updateOne({
-    _id: 2
+  _id: 2
 }, {
-    $inc: {
-        quantity: -2
-    }
+  $inc: {
+    quantity: -2
+  }
 });
 session.abortTransaction();
 session.endSession();
 
 
 // Sample commit transaction
-var session = db.getMongo().startSession( { readPreference: { mode: "primary" } } );
-session.startTransaction( { readConcern: { level: "majority" }, writeConcern: { w: "majority" } } );
+var session = db.getMongo().startSession({ readPreference: { mode: "primary" } });
+session.startTransaction({ readConcern: { level: "majority" }, writeConcern: { w: "majority" } });
 session.getDatabase("test").orders.insertOne({
-    _id: new ObjectId(),
-    total: new NumberLong(8000),
-    items: [
-        {
-            product_id: 1,
-            price: new NumberLong(2000),
-            quantity: new NumberInt(2)
-        },
-        {
-            product_id: 2,
-            price: new NumberLong(2000),
-            quantity: new NumberInt(2)
-        }
-    ]
+  _id: new ObjectId(),
+  total: new Long(8000),
+  items: [
+    {
+      product_id: 1,
+      price: new Long(2000),
+      quantity: new NumberInt(2)
+    },
+    {
+      product_id: 2,
+      price: new Long(2000),
+      quantity: new NumberInt(2)
+    }
+  ]
 });
 session.getDatabase("test").products.updateOne({
-    _id: 1
+  _id: 1
 }, {
-    $inc: {
-        quantity: -2
-    }
+  $inc: {
+    quantity: -2
+  }
 });
 session.getDatabase("test").products.updateOne({
-    _id: 2
+  _id: 2
 }, {
-    $inc: {
-        quantity: -2
-    }
+  $inc: {
+    quantity: -2
+  }
 });
 session.commitTransaction();
 session.endSession();
